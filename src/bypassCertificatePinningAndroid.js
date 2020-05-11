5ef05805684de92032262af9131a7d4ff1e8c0e3
Cees Mandjes <ceesmandjes@gmail.com>
2020-05-11 18:12:49 +0200

    Fixed typos

--- src/bypassCertificatePinningAndroid.js
+++ src/bypassCertificatePinningAndroid.js
@@ -1,5 +1,5 @@
 /*
-    Bypass certificate pinning script for Android's default library and the OKHttpv3 library. Using this script all certificates will be considered as trusted.
+    Bypass certificate pinning script for Android's default library and the OKHttp library. Using this script all certificates will be considered as trusted.
 
     $ frida -U -l bypassCertificatePinningAndroid.js -f org.package.name --no-pause
 
@@ -7,23 +7,23 @@
 */
 Java.perform(function (){
     console.log("");
-    console.log("-- Bypass certificate pinning for Android's default libary and OKHTTPv3 --");
+    console.log("-- Bypass certificate pinning for Android's default libary and OKHttp --");
 
-        //Bypass the OKHttpv3 Library
+	//Bypass the OKHttp Library
     try {
-        console.log("Looking for OKHTTPv3 library");
+        console.log("Looking for OKHttp library");
         //Class config
         var CertificatePinnerBuilder = Java.use('okhttp3.CertificatePinner$Builder');
 
         CertificatePinnerBuilder.add.implementation = function(host, pins) {
-            console.log("OKHTTPv3: remove pin for host: " + host);
+            console.log("OKHttp: remove pin for host: " + host);
             var fakeHostName = "x";
             return this.add(fakeHostName, pins);
         }
 
-        console.log("OKHTTPv3 library was succesfully hooked");
+        console.log("OKHttp library was succesfully hooked");
     } catch (e) {
-        console.log("OKHTTPv3 library was not found");
+        console.log("OKHttp library was not found");
     }
 
     //Bypass the Android's default Library
@@ -49,14 +49,6 @@ Java.perform(function (){
 
         //Skip certificate chain validation by returning the received certificate chain as a trusted chain
         TrustManagerImpl.checkTrustedRecursive.implementation = function(certs, ocspData, tlsSctData, host, clientAuth, untrustedChain, trustAnchorChain, used) {
-            if(host != null)
-                console.log("Android: skip chain validation for host:  "+ host);
-            else 
-            {
-                console.log("Host is null");
-                return Java.use('java.util.ArrayList').$new();
-            }
-
             var certsList = Java.use('java.util.ArrayList').$new();
             var i;
             for (i = 0; i < certs.length; i++) {               
@@ -71,8 +63,7 @@ Java.perform(function (){
     }
 
     //Information loging for Android SafetyNet
-    try
-    {
+    try {
         console.log("Looking for Android SafetyNet Attestation library");
         //Classes config
         var SafetyNetClient = Java.use("com.google.android.gms.safetynet.SafetyNetClient");
@@ -81,8 +72,7 @@ Java.perform(function (){
         var String = Java.use("java.lang.String");
     
         //Logs when SafetyNet Attestation evaluation starts and continues its regular behavior
-        SafetyNetClient.attest.implementation = function(nonce, apiKey)
-        {
+        SafetyNetClient.attest.implementation = function(nonce, apiKey) {
             console.log("SafetyNet Attestation is called");            
             return this.attest(nonce, apiKey);
         }
@@ -97,9 +87,7 @@ Java.perform(function (){
         }
 
         console.log("Android SafetyNet Attestation library was succesfully hooked");
-
-    }
-    catch (e) {
+    } catch (e) {
         console.log("Android SafetyNet Attestation library was not found");
     }
 
